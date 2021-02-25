954666e73bc75518192cd2d1262419cb555a1689
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-02-25 14:16:11 -0500

    Adding webpack

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,12 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
+    '/public/dist/',
+    '/models/transaction.js',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-512x512.png',
+    '/public/styles.css',
+    
 
   ];
   
